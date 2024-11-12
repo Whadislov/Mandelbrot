136a78e55ca89c4eabd92eb7929d69f5c59ba0eb
Julien Morisse <morissejulien76110@gmail.com>
2024-11-12 16:30:40 +0100

    Last version with colors and zooms

--- projet_mandelbrot.py
+++ projet_mandelbrot.py
@@ -9,8 +9,10 @@ import numpy as np
 import warnings
 import matplotlib.pyplot as plt
 from PIL import Image
+from PIL import ImageEnhance
 from dataclasses import dataclass
 from mandelbrot import MandelbrotSet
+from viewport import Viewport
 
 warnings.filterwarnings("ignore")
 
@@ -42,27 +44,67 @@ plt.axis("off")
 plt.tight_layout()
 plt.show()
 '''
-mandelbrot_set = MandelbrotSet(max_iterations=30, escape_radius=1000)
 
-width, height = 600, 600
-scale = 0.0075
+'''
+mandelbrot_set = MandelbrotSet(max_iterations=256, escape_radius=1000)
+
+width, height = 540, 350
+scale = 0.00075
 BLACK_AND_WHITE = "1"
 GREY = "L" #luminance (range from 0 to 255)
-
+'''
 '''
 image = Image.new(mode=BLACK_AND_WHITE, size=(width, height))
 for y in range(height):
     for x in range(width):
         c = scale * complex(x - width / 2, height / 2 - y)
         image.putpixel((x, y), c not in mandelbrot_set) #white = c not in the set. putpixel expects a numerical value, bools in python are a subclass of integer
-'''
+
 image = Image.new(mode=GREY, size=(width, height))
 for y in range(height):
     for x in range(width):
         c = scale * complex(x - width / 2, height / 2 - y)
         instability = 1 - mandelbrot_set.stability(c, smooth=True) #stable = black, instable = white
         image.putpixel((x, y), int(instability * 255))
-
 image.show()
+'''
+
+
+'''
+for pixel in Viewport(image, center=-0.7435 + 0.1314j, width=0.002):
+    c = complex(pixel)
+    instability = 1 - mandelbrot_set.stability(c, smooth=True)
+    pixel.color = int(instability * 255)
+'''
+
+mandelbrot_set = MandelbrotSet(max_iterations=512, escape_radius=1000)
+
+#Helper function
+def paint(mandelbrot_set, viewport, palette, smooth):
+    for pixel in viewport:
+        stability = mandelbrot_set.stability(complex(pixel), smooth)
+        index = int(min(stability * len(palette), len(palette) - 1))
+        pixel.color = palette[index % len(palette)]
+
+#converts a normalized tuple (0.2, 0.11, 0.2) to (51, 28, 51), as Pillow values must range between 0 and 255
+def denormalize(palette):
+    return [
+        tuple(int(channel * 255) for channel in color)
+        for color in palette
+    ]
+
+#Get a colormap from matplotlib (normalized)
+import matplotlib.cm
+colormap = matplotlib.cm.get_cmap("twilight").colors
+palette = denormalize(colormap)
+
+image = Image.new(mode="RGB", size=(512, 512))
+viewport = Viewport(image, center=-0.7435 + 0.1314j, width=0.002)
+paint(mandelbrot_set, viewport, palette, smooth=True)
+image.show()
+
+
+
+
 
 
